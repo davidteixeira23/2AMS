e4f149c184dc526c7e10952c3deade8bac2f2310
David Teixeira <davidpedreira250@gmail.com>
2026-05-17 09:41:12 -0300

    Add files via upload

---
+++ PHP/Exercicio1-Edina/Index.php
@@ -0,0 +1,21 @@
+<html>
+
+<head>
+	<title> Exemplo de funcionamento do PHP </title>
+</head>
+
+<body>
+	<?php
+	echo "David Pedreira de Souza Teixeira";
+	?>
+	<img src="C:\Users\David\OneDrive\Imagens\Documentos">
+	<br>
+
+	<?php
+	echo "16 anos";
+
+
+	?>
+</body>
+
+</html>
