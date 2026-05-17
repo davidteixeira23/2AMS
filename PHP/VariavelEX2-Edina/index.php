8d3375cf6b11941ab10b4b73bb2e2f5eb2ad4cf3
David Teixeira <davidpedreira250@gmail.com>
2026-05-17 10:29:41 -0300

    Add files via upload

---
+++ PHP/VariavelEX2-Edina/index.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <?php
+
+    $nome = "David";
+    $sobrenome = "Teixeira";
+    $idade = 16;
+    $nascimento = "23/10/2009";
+
+    echo "Meu nome é " . $nome;
+    echo " " . $sobrenome;
+    echo " e tenho " . $idade . " anos.";
+    echo " Nascido em " . $nascimento;
+
+
+
+
+
+
+    ?>
+
+
+
+
+</body>
+
+</html>
