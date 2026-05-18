0eaf28e7e18de016b12d54fcc183e95c06ec017d
David Teixeira <davidpedreira250@gmail.com>
2026-05-18 17:59:01 -0300

    Add files via upload

---
+++ PHP/Porcentagem.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<!--receba e calcule 15% do valor digitado pelo usuario, -->
+<form action ="acao.php" method="post">
+<seu 
+
+
+</form>
+
+
+
+
+
+
+</body>
+</html>
