0d926ce11a390ed8b9c8bab49fab44b5c5e24667
David Teixeira <davidpedreira250@gmail.com>
2026-05-22 22:43:40 -0300

    Add files via upload

---
+++ PHP/Exercicios Marcelo 2 Bimestre/Desconto/Index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+
+    <form action='Desconto.php' method="POST">
+        Digite o Preço do Produto: <input type='number' name='preco'><br><br>
+        Digite a porcentagem de desconto do protudo <input type='number' name='desconto'><br><br>
+        <input type='submit' value='Calcular'>
+    </form>
+
+
+</body>
+
+</html>
