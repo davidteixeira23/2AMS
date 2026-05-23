53ea50d7491fc2160860428db265c8b5f8f48621
David Teixeira <davidpedreira250@gmail.com>
2026-05-22 21:47:55 -0300

    Add files via upload

---
+++ PHP/Exercicios Marcelo 2 Bimestre/Multiplicação em php/index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+
+    <form action="multiplicar.php" method="POST">
+        Digite o número que da multiplicação <input type='number' name='num1'>
+        <button type="submit" value='MUltiplicar'>Multiplicar</button>
+    </form>
+
+
+
+</body>
+
+</html>
---
+++ PHP/Exercicios Marcelo 2 Bimestre/Multiplicação em php/multiplicar.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <title>Tabuada</title>
+</head>
+
+<body>
+
+    <?php
+
+    $num1 = $_POST['num1'];
+
+    echo "<h1>Tabuada do $num1</h1>";
+
+    echo $num1 * 1 . "<br>";
+    echo $num1 * 2 . "<br>";
+    echo $num1 * 3 . "<br>";
+    echo $num1 * 4 . "<br>";
+    echo $num1 * 5 . "<br>";
+    echo $num1 * 6 . "<br>";
+    echo $num1 * 7 . "<br>";
+    echo $num1 * 8 . "<br>";
+    echo $num1 * 9 . "<br>";
+    echo $num1 * 10 . "<br>";
+
+    ?>
+
+</body>
+
+</html>
