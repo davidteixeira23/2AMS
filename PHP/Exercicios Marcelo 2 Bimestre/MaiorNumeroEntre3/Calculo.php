0b32e16e0f9a317d2292ef5ec208d487f8f3a1d9
David Teixeira <davidpedreira250@gmail.com>
2026-05-24 15:47:19 -0300

    Add files via upload

---
+++ PHP/Exercicios Marcelo 2 Bimestre/MaiorNumeroEntre3/Calculo.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+
+    <?php
+    $n1 = $_POST['n1'];
+    $n2 = $_POST['n2'];
+    $n3 = $_POST['n3'];
+
+    if ($n1 > $n2 || $n2 > $n3) {
+        echo "O numero 1 é maior que numero 2 que é maior que numero 3";
+    } else if ($n1 > $n3 || $n3 > $n2) {
+        echo "O numero 1 é maior que o numero 3 que é maior que o numero 2";
+    } else if ($n2 > $n1 || $n1 > $n3) {
+        echo "O numero 2 é maior que o numero1 que é maior que o numero 3";
+    } else if ($n2 > $n3 || $n3 > $n1) {
+        echo "O numero 2 é maior que o numero 3 que é maior que o numero2";
+    } else if ($n3 > $n2 || $n2 > $n1) {
+        echo "O numero 3 é maior que o numero 2 que é maior que o numero3";
+    } else if ($n3 > $n2 || $n2 > $n1) {
+        echo "O numero 3 é maior que o numero 2 que é maior que o numero 1";
+    }
+
+
+
+    ?>
+
+
+
+</body>
+
+</html>
---
+++ PHP/Exercicios Marcelo 2 Bimestre/MaiorNumeroEntre3/index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <form action="Calculo.php" method="post">
+        numero1 <input type="number" name="n1"> <br><br>
+        numero2 <input type="number" name="n2"> <br><br>
+        numero3 <input type="number" name="n3"> <br><br>
+        <input type="submit" value="Testar ">
+
+
+
+
+    </form>
+
+
+
+</body>
+
+</html>
