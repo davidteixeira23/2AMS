a02da75ba3572094378970b35ac963276674ff45
David Teixeira <davidpedreira250@gmail.com>
2026-05-24 09:18:00 -0300

    Update MyDataBase.sql

--- Banco de Dados/MyDataBase.sql
+++ Banco de Dados/MyDataBase.sql
@@ -12,6 +12,6 @@ login varchar (15) not null unique,
 senha varchar (15) not null
 );
 
--- rexderwxwexw3essedxdsewxwzwsxdzxdxxedxsdse12
+-- rexderwxwexw3essedxdsewxwzwsxdzxdxxedxsdse123
 -- o comando abaixo descreve a tabela 
 describe tbusuarios;
