8af5f6d2799c6d5e316f25fec64b77d236c75ef4
David Teixeira <davidpedreira250@gmail.com>
2026-05-24 13:05:33 -0300

    Add files via upload

---
+++ PHP/Exercicios Marcelo 2 Bimestre/TrocadeVariaveis/Calculo.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <?php
+    $_num1inver = $_POST['var2'];
+    $_num2inver = $_POST['var1'];
+
+    echo "valor da primeira variavel : " . $_num1inver;
+    echo "<br>";
+    echo "Valor da segunfa variavel :" .  $_num2inver;
+
+
+
+    ?>
+
+</body>
+
+</html>
---
+++ PHP/Exercicios Marcelo 2 Bimestre/TrocadeVariaveis/index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+
+    <form action="Calculo.php" method="post">
+        Digite o valor do primeiro numero <input type="number" name="var1"> <br>
+        Digite o valor do segundo numero <input type="number" name="var2"> <br>
+        <input type="submit" value="calcular">
+
+
+
+
+
+
+    </form>
+
+
+
+</body>
+
+</html>
