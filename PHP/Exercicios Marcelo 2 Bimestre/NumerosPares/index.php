6823c3d79a5527db9f8076d69535513fcc93e52d
David Teixeira <davidpedreira250@gmail.com>
2026-05-30 21:02:40 -0300

    Update index.php

--- PHP/Exercicios Marcelo 2 Bimestre/NumerosPares/index.php
+++ PHP/Exercicios Marcelo 2 Bimestre/NumerosPares/index.php
@@ -8,7 +8,7 @@
 </head>
 
 <body>
-
+  <h1>Descubra se um número é par ou impar.</h1><br><br>
     <form action="Calculo.php" method="post">
         Digite o valor do numero <input type="number" name="n1"> <br> <br>
         <input type="submit" value="Conferir">
